6055376649692663fdcb0d202b94597f8fc5389a
mentuly <danlutsencko@gmail.com>
2025-05-05 11:34:34 +0300

    update main

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from flask import Flask, request
 import telebot
 import os
+import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-@app.route('https://api.telegram.org/bot7030634417:AAH8AARR1ARkOGMe2mhRiI7TKABH8Yn0Pzc/setWebhook?url=https://your-app-name.up.railway.app/', methods=['POST'])
+@app.before_first_request
+def set_webhook():
+    webhook_url = f"{WEBHOOK_URL}"
+    response = WEBHOOK_URL = "https://web-production-8dd7d.up.railway.app/"
+
+requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}")
+    print(response.json())
+
+@app.route('/', methods=['POST'])
 def webhook():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
@@ -22,4 +33,4 @@ def echo_all(message):
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
