b4bb3b39f62d0ba4d667cdede8dfd56c4beb7345
mentuly <danlutsencko@gmail.com>
2025-05-05 11:15:58 +0300

    webhook

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('https://api.telegram.org/bot7030634417:AAH8AARR1ARkOGMe2mhRiI7TKABH8Yn0Pzc/setWebhook?url=https://your-app-name.up.railway.app/', methods=['POST'])
 def webhook():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
