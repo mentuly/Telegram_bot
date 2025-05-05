4cdbf9c21ca681f94cc5d5cd14f3aa691fe093ec
mentuly <danlutsencko@gmail.com>
2025-05-05 15:35:08 +0300

    some changes

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
+# from flask import Flask, request
+# import telebot
+# import os
+# import requests
+
+# TOKEN = os.getenv("BOT_TOKEN")
+# WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+# bot = telebot.TeleBot(TOKEN)
+# app = Flask(__name__)
+
+# @app.before_first_request
+# def set_webhook():
+#     webhook_url = f"{WEBHOOK_URL}"
+#     response = WEBHOOK_URL = "https://web-production-8dd7d.up.railway.app/"
+
+
 from flask import Flask, request
 import telebot
 import os
-import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-
 bot = telebot.TeleBot(TOKEN)
+WEBHOOK_URL = 'https://web-production-8dd7d.up.railway.app/'  # <- заміниш після деплою
+
 app = Flask(__name__)
-
-@app.before_first_request
-def set_webhook():
-    webhook_url = f"{WEBHOOK_URL}"
-    response = WEBHOOK_URL = "https://web-production-8dd7d.up.railway.app/"
-
-requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}")
-    print(response.json())
-
-@app.route('/', methods=['POST'])
-def webhook():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return 'OK', 200
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привіт! Я бот на Railway!")
+    bot.send_message(message.chat.id, 'Привіт!')
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(content_types=['text'])
+def get_txt_message(message):
+    bot.send_message(message.from_user.id, message.text)
+
+@app.route('/', methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return '!', 200
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Бот працює!', 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
