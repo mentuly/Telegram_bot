a1527c243d2cf4cac4830d92d3e513c8a57ecdf1
mentuly <danlutsencko@gmail.com>
2025-05-08 11:18:50 +0300

    some new changes, switch telebot to aiogram

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
-# from flask import Flask, request
-# import telebot
-# import os
-# import requests
-
-# TOKEN = os.getenv("BOT_TOKEN")
-# WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-
-# bot = telebot.TeleBot(TOKEN)
-# app = Flask(__name__)
-
-# @app.before_first_request
-# def set_webhook():
-#     webhook_url = f"{WEBHOOK_URL}"
-#     response = WEBHOOK_URL = "https://web-production-8dd7d.up.railway.app/"
-
-
-from flask import Flask, request
-import telebot
 import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
 
 TOKEN = os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(TOKEN)
 WEBHOOK_URL = 'https://web-production-8dd7d.up.railway.app/'  # <- заміниш після деплою
 
-app = Flask(__name__)
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Привіт!')
+@dp.message(commands=["start"])
+async def start_handler(message: Message):
+    await message.answer("Привіт!")
 
-@bot.message_handler(content_types=['text'])
-def get_txt_message(message):
-    bot.send_message(message.from_user.id, message.text)
+@dp.message()
+async def echo_handler(message: Message):
+    await message.answer(message.text)
 
-@app.route('/', methods=['POST'])
-def webhook():
-    json_str = request.get_data().decode('UTF-8')
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return '!', 200
+async def on_startup(app: web.Application):
+    await bot.set_webhook(WEBHOOK_URL)
 
-@app.route('/', methods=['GET'])
-def index():
-    return 'Бот працює!', 200
+async def on_shutdown(app: web.Application):
+    await bot.delete_webhook()
 
-if __name__ == '__main__':
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL)
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+async def index(request):
+    return web.Response(text="Бот працює!", status=200)
+
+app = web.Application()
+app.router.add_get("/", index)
+
+# Налаштовуємо webhook
+SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path="/")
+app.on_startup.append(on_startup)
+app.on_shutdown.append(on_shutdown)
+
+if __name__ == "__main__":
+    web.run_app(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
