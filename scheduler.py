92ae3e432d18e9e9c3b6e20ab8ba473c8b625f8d
mentuly <danlutsencko@gmail.com>
2025-05-10 21:30:04 +0300

    test on 21:35

--- scheduler.py
+++ scheduler.py
@@ -3,5 +3,5 @@ from handlers.daily import send_daily_message
 
 def setup_scheduler(bot, user_id: int):
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_daily_message, "cron", hour=9, minute=0, args=[bot, user_id])  # щодня о 09:00
+    scheduler.add_job(send_daily_message, "cron", hour=18, minute=35, args=[bot, user_id])
     scheduler.start()
